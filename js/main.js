ed54f54893fd9755f4ba6d5d6b87ce6d837daed9
Smaran Ummadishetty <speak2smaran@gmail.com>
2024-07-31 13:54:54 +0530

    added new pages

--- js/main.js
+++ js/main.js
@@ -121,3 +121,75 @@ $(".nav__sub").click(function () {
     $(this).removeClass("active");
   }
 });
+function incrementValue(e) {
+  e.preventDefault();
+  var fieldName = $(e.target).data('field');
+  var parent = $(e.target).closest('div');
+  var currentVal = parseInt(parent.find('input[name=' + fieldName + ']').val(), 10);
+
+  if (!isNaN(currentVal)) {
+    parent.find('input[name=' + fieldName + ']').val(currentVal + 1);
+  } else {
+    parent.find('input[name=' + fieldName + ']').val(0);
+  }
+}
+
+function decrementValue(e) {
+  e.preventDefault();
+  var fieldName = $(e.target).data('field');
+  var parent = $(e.target).closest('div');
+  var currentVal = parseInt(parent.find('input[name=' + fieldName + ']').val(), 10);
+
+  if (!isNaN(currentVal) && currentVal > 0) {
+    parent.find('input[name=' + fieldName + ']').val(currentVal - 1);
+  } else {
+    parent.find('input[name=' + fieldName + ']').val(0);
+  }
+}
+
+$('.input-group').on('click', '.button-plus', function(e) {
+  incrementValue(e);
+});
+
+$('.input-group').on('click', '.button-minus', function(e) {
+  decrementValue(e);
+});
+window.addEventListener('load',toggleForm('credit'))
+function toggleForm(e){
+  document.getElementById('formType').innerHTML = e === 'credit' ? `<div class="form-floating mt-3">
+    <input id="nameOnCard" type="text" class="form-control">
+    <label for="nameOnCard">Name On Card</label>
+  </div>
+  <div class="form-floating mt-3">
+    <input id="cardNumber" type="text" class="form-control">
+    <label for="cardNumber">Card Number</label>
+  </div>
+  <div class="row g-3">
+    <div class="col">
+      <div class="form-floating mt-3">
+        <input id="expiry_mm" type="text" class="form-control">
+        <label for="expiry_mm">Expiry (mm)</label>
+      </div>
+      </div>
+        <div class="col">
+          <div class="form-floating mt-3">
+            <input id="cvv" type="text" class="form-control">
+            <label for="cvv">CVV</label>
+          </div>
+        </div>
+      </div>` : `<div class="form-floating mt-3">
+        <input id="accountHolder" type="text" class="form-control">
+        <label for="accountHolder">Name of Account Holder</label>
+      </div>
+      <div class="form-floating mt-3">
+        <input id="routing" type="text" class="form-control">
+        <label for="routing">Routing Number</label>
+      </div>
+      <div class="form-floating mt-3">
+        <select name="accountType" id="accountType" class="form-control">
+          <option value="checking">Checking</option>
+          <option value="savings">Savings</option>
+        </select>
+        <label for="accountType">Account Type</label>
+      </div>`
+    }
